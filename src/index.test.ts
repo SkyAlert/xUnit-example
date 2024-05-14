8336d95ae4317c218aad289c50bb799ff36b07fd
whtlnv <catala.francisco@gmail.com>
2024-05-13 20:29:49 -0600

    our testless solution

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,3 @@
-import { sum } from './index';
-
-describe('When adding two numbers', () => {
-  it('should return the sum', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
+test('dummy', () => {
+  expect(true).toBe(true);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
+// da code
 export const sum = (a: number, b: number): number => a + b;
+
+// xUnit
+const xTest = {
+  run: (name: string, test: () => void) => {
+    try {
+      test();
+      console.log(`✅ ${name}`);
+    } catch (err) {
+      console.log(`❌ ${name}`);
+    }
+  },
+};
+
+// Tests
+xTest.run('adds 1 + 2 to equal 3', () => {
+  if (sum(1, 2) !== 3) {
+    throw new Error('Test failed');
+  }
+});
+
+xTest.run('adds 1 + 3 to equal 4', () => {
+  if (sum(1, 3) !== 4) {
+    throw new Error('Test failed');
+  }
+});
