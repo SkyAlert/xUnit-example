3cfcea52b23648849711a1abca2ff805f61b5238
Mario Ferreira <ztrange@gmail.com>
2024-05-20 20:19:45 -0600

    Chapter 19

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,11 @@ import assert from 'assert';
 class TestCase {
   constructor(public name: string) {}
 
+  setUp() {}
+
   run() {
+    this.setUp();
+
     // @ts-ignore: I know what im doing
     if (this[this.name] instanceof Function) {
       // @ts-ignore: I really know what im doing
@@ -18,11 +22,17 @@ class TestCase {
 }
 
 class WasRun extends TestCase {
-  public wasRun: boolean;
+  public wasRun: boolean | undefined;
+  public wasSetUp: boolean;
 
   constructor(name: string) {
     super(name);
+    this.wasSetUp = false;
+  }
+
+  override setUp() {
     this.wasRun = false;
+    this.wasSetUp = true;
   }
 
   testMethod() {
@@ -31,14 +41,28 @@ class WasRun extends TestCase {
 }
 
 class TestCaseTest extends TestCase {
+  private test: WasRun | undefined;
+
+  override setUp(): void {
+    this.test = new WasRun('testMethod');
+  }
+
+  testSetUp() {
+    assert(this.test);
+    this.test.run();
+    assert(this.test.wasSetUp);
+  }
+
   // eslint-disable-next-line class-methods-use-this
   testRunning() {
-    const myTest = new WasRun('testMethod');
-    assert(!myTest.wasRun);
-    myTest.run();
-    assert(myTest.wasRun);
+    assert(this.test);
+    this.test.run();
+    assert(this.test.wasRun);
   }
 }
 
 const test = new TestCaseTest('testRunning');
 test.run();
+
+const test2 = new TestCaseTest('testSetUp');
+test2.run();
